f191d1d5469a48cae94ba75dcc25489fb8daa560
okhuz <haoralk@gmail.com>
2020-02-05 22:04:53 +0300

    DTO Updates

--- src/app/dtos/enums.ts
+++ src/app/dtos/enums.ts
@@ -1,21 +1,36 @@
 export enum UserType {
-    Basic,
-    Premium,
-    Admin
+    Basic = 1,
+    Premium = 2,
+    Admin = 4
 }
 
 export enum Gender {
-    Male,
-    Female,
-    Gay,
-    Lesbian,
-    Pangender,
-    Bisexual,
-    Asexual
+    Male = 1,
+    Female = 2,
+    Gay = 4,
+    Lesbian = 8,
+    Pangender = 16,
+    Bisexual = 32,
+    Asexual = 64
 }
 
-
 export enum RoomType {
-    Public,
-    Private
+    Public = 1,
+    Private = 2
+}
+
+export enum SchoolType {
+    Elementary = 1,
+    HighSchool = 2,
+    University = 4,
+    Pavement = 8
+}
+
+export enum RelationshipStatus {
+    Married = 1,
+    Single = 2,
+    InRelationShip = 4,
+    InOpenRelationShip = 8,
+    Divorced = 16,
+    Complicated = 32,
 }
--- src/app/dtos/user.ts
+++ src/app/dtos/user.ts
@@ -1,5 +1,5 @@
 import { UserSettings } from './UserSettings';
-import { UserType, Gender } from './enums';
+import { UserType, Gender, SchoolType, RelationshipStatus } from './enums';
 
 export class User {
     email: string;
@@ -7,10 +7,13 @@ export class User {
     name: string;
     surname: string;
     description:string;
+    schoolType: SchoolType;
+    school: string;
     birthday: Date;
     id: string;
     userType: UserType;
     userSetting: UserSettings;
     gender: Gender;
+    relationshipStatus: RelationshipStatus
 }
 
