632d2de5385822f8466602af5b1827b174ae02f4
okhuz <haoralk@gmail.com>
2020-02-13 01:32:59 +0300

    routers with ids

---
+++ src/app/pages/event-page/event-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { EventService } from 'src/app/services/dataServices/event-service.service';
+import { Event } from '../../dtos/event';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-event-page',
+  templateUrl: './event-page.component.html',
+  styleUrls: ['./event-page.component.scss']
+})
+export class EventPageComponent implements OnInit {
+
+  event$: Observable<any>;
+  event: Event;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private eventService: EventService
+  ) { }
+
+  ngOnInit(): void {
+    let eventId = this.route.snapshot.paramMap.get('id');
+    this.event$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.eventService.getEventById(params.get('id')))
+    );
+    this.event$.subscribe(p => this.event = Object.values(p)[0] as Event);
+  }
+
+}
