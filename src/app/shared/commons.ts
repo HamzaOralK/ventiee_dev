23c64c8288e9eed3bb0bdc9315836b703000accb
okhuz <haoralk@gmail.com>
2020-06-11 10:32:37 +0300

    Type fix.

--- src/app/shared/commons.ts
+++ src/app/shared/commons.ts
@@ -29,7 +29,7 @@ export namespace COMMONS {
       r = parseInt(trim(inParts[0].substring(1)), 10),
       g = parseInt(trim(inParts[1]), 10),
       b = parseInt(trim(inParts[2]), 10),
-      a = parseFloat(trim(inParts[3].substring(0, inParts[3].length - 1)).toFixed(2));
+      a = parseFloat(trim(inParts[3].substring(0, inParts[3].length - 1))).toFixed(2) as any;
     var outParts = [
       r.toString(16),
       g.toString(16),
